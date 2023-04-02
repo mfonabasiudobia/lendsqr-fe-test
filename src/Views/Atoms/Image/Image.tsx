dd35f2cd241c304233143b7a3c77522de6c2ed94
MfonAbasi Udobia <mfonabasiisaac@fmail.com>
2023-04-02 10:37:24 +0100

    Added Image Atom

---
+++ src/Views/Atoms/Image/Image.tsx
@@ -0,0 +1,45 @@
+import { ImageModel } from "./Models/Image";
+import useAtomicStyle from "@/Views/Hooks/useAtomicStyle";
+import Image from "next/image";
+
+const Avatar: React.FC<ImageModel> = ({
+  className,
+  variant,
+  elevation,
+  margin,
+  padding,
+  alt,
+  src,
+  objectFit = "contain",
+  fill = false,
+  width = 0,
+  height = 0,
+}) => {
+  const styles = {
+    importedStyles: useAtomicStyle({ margin, padding, className, elevation }),
+    variantClass: `${
+      variant === "circle"
+        ? "rounded-full"
+        : variant === "square"
+        ? `rounded-0`
+        : variant === "rounded"
+        ? "rounded"
+        : null
+    }`,
+    objectFit: objectFit ? `object-${objectFit}` : null,
+  };
+
+  return (
+    <Image
+      alt={alt}
+      src={src}
+      width={width}
+      priority={true}
+      height={height}
+      fill={fill}
+      className={`${Object.values(styles).join(" ")}`}
+    />
+  );
+};
+
+export default Avatar;
---
+++ src/Views/Atoms/Image/Models/Image.ts
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+import { StyleModel } from "@/Views/Hooks/Models";
+import { StaticImageData } from "next/image";
+
+
+export interface ImageModel extends StyleModel {
+  variant: "circle" | "square" | "rounded";
+  alt: string;
+  src: string | StaticImageData;
+  fill: boolean,
+  objectFit?: "fill" | "cover" | "contain",
+  width?: number,
+  height?: number
+}
