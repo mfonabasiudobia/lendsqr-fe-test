a81a6e088fd83f4bbe3bfbd5d3c7a5ff2a00ecbf
MfonAbasi Udobia <mfonabasiisaac@fmail.com>
2023-04-04 19:06:29 +0100

    Added Type hinting to getServerProps

--- src/pages/dashboard/users/index.tsx
+++ src/pages/dashboard/users/index.tsx
@@ -1,4 +1,5 @@
 import Users from "@/Views/Pages/Dashboard/Users/Users";
+import { GetServerSideProps } from "next";
 import { axios } from "@config";
 
 interface UserPageModel {
@@ -6,17 +7,13 @@ interface UserPageModel {
 }
 
 const UsersPage: React.FC<UserPageModel> = ({ users }) => {
-  return (
-    <>
-      <Users users={users} />
-    </>
-  );
+  return <Users users={users} />;
 };
 
-export async function getServerSideProps() {
+export const getServerSideProps: GetServerSideProps = async () => {
   const { data } = await axios.get("users");
 
   return { props: { users: data } };
-}
+};
 
 export default UsersPage;
